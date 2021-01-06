6515d9b6da6e6b3f8448757da5d4d5b9e711a949
Paulyne <paulynewambui2020@gmail.com>
2021-01-06 11:48:01 +0300

    add the login endpoint

--- playstation/serializers.py
+++ playstation/serializers.py
@@ -18,3 +18,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username')
+        
+class LoginUserSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("error 400 Bad request.Invalid Details, key in the correct credentials.")
--- virtual/bin/django-admin.py
+++ virtual/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/rustique/Desktop/Dean's work/Registration-B/virtual/bin/python3
+#!/home/wambui/Desktop/Playstation-backend/virtual/bin/python3
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
